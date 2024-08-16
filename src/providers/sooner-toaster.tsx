364fdd7784701c6fe4dbea20c4f10d119020de2c
Faisal Tariq <faisaltariq183@gmail.com>
2024-08-16 23:09:37 +0400

    added toaster

--- src/providers/sooner-toaster.tsx
+++ src/providers/sooner-toaster.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaCircleCheck, FaCircleInfo } from "react-icons/fa6";
+import { MdOutlineError } from "react-icons/md";
+import { TiWarning } from "react-icons/ti";
 import { Toaster, type ToasterProps } from "sonner";
 
 const SoonerToaster = ({ ...props }: ToasterProps) => {
@@ -6,7 +9,17 @@ const SoonerToaster = ({ ...props }: ToasterProps) => {
     <Toaster
       richColors
       position="bottom-right"
-      toastOptions={{ classNames: { content: "text-sm" } }}
+      toastOptions={{
+        classNames: {
+          content: "text-sm font-sans",
+        },
+      }}
+      icons={{
+        success: <FaCircleCheck className="size-5" />,
+        info: <FaCircleInfo className="size-5" />,
+        warning: <TiWarning className="size-5" />,
+        error: <MdOutlineError className="size-5" />,
+      }}
       {...props}
     />
   );
