787b6bed391e35a752b3ac3539fdafac29780dc5
Faisal Tariq <faisaltariq183@gmail.com>
2024-08-30 17:03:30 +0400

    chore: add next/bundle-analyzer for bundle size analysis

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-import { fileURLToPath } from 'node:url'
 import createJiti from 'jiti'
+import { fileURLToPath } from 'node:url'
+
+import withBundleAnalyzer from '@next/bundle-analyzer'
 const jiti = createJiti(fileURLToPath(import.meta.url))
 
 jiti('./src/constant/env.ts')
 
-/** @type {import("next").NextConfig} */
-const config = {}
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-export default config
+/** @type {import("next").NextConfig} */
+const nextConfig = {}
+
+export default bundleAnalyzer(nextConfig)
