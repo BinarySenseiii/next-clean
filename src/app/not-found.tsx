ff455d95a0bbd391c7a17954f286ddafcd3e5088
Faisal Tariq <faisaltariq183@gmail.com>
2024-08-31 14:01:47 +0400

    chore(pages): errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import { Frown } from 'lucide-react'
 import Link from 'next/link'
 
 import { getBackgroundPattern } from '~/components/bg-patterns'
@@ -8,25 +6,21 @@ import { Button } from '~/components/design-system/button'
 const NotFoundPage = () => {
   return (
     <div className='relative grid min-h-dvh place-content-center'>
-      {getBackgroundPattern('bg-light-grid-1')}
+      {getBackgroundPattern('bg-light-grid-gradient-1')}
 
-      <div className='container'>
-        <div className='flex flex-col items-center justify-center text-center'>
-          <Frown className='size-16 text-gray-600' />
-          <h1 className='mt-4 text-balance text-3xl font-black italic'>
-            This page doesn&apos;t exist
-          </h1>
-          <p className='mx-auto my-4 max-w-xl text-center text-base'>
-            Oops! It seems like you&apos;ve stumbled upon a page that doesn&apos;t exist Don&apos;t
-            worry, even the best of us get lost sometimes
-          </p>
+      <div className='flex flex-col items-center justify-center text-center'>
+        <h1 className='mt-4 text-balance text-7xl font-black sm:text-8xl'> 404 </h1>
+        <h2 className='mt-4 text-balance text-2xl font-black sm:text-3xl'>Page not found</h2>
+        <p className='mx-auto my-4 max-w-xl text-balance text-center text-sm sm:text-base'>
+          It looks like the page you&apos;re searching for doesn’t exist. The link might be broken
+          or the page may have been moved
+        </p>
 
-          <Button asChild>
-            <Link href='/'>
-              <span className='text-xs md:text-base'>Back to home</span>
-            </Link>
-          </Button>
-        </div>
+        <Button asChild>
+          <Link href='/'>
+            <span className='text-xs md:text-base'>Back to home</span>
+          </Link>
+        </Button>
       </div>
     </div>
   )
--- src/lib/shadcn-plugin.ts
+++ src/lib/shadcn-plugin.ts
@@ -73,7 +73,7 @@ export const shadcnPlugin = plugin(
     theme: {
       container: {
         center: true,
-        padding: '2rem',
+        padding: '1rem',
         screens: {
           '2xl': '1400px',
         },
