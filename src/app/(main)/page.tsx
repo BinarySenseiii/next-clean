bcab344a1d19d5534cde098881253e873578b868
Faisal Tariq <faisaltariq183@gmail.com>
2024-09-15 21:07:24 +0400

    chore: template-ui

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,30 @@
 'use client'
+
 import { Button } from '~/core/components/design-system/button'
-import { useBearActions, useBears } from '~/core/store/bear-store'
+import { useCount, useCountActions } from '~/core/store/count-store'
 
 export default function HomePage() {
-  const bears = useBears()
-  const { increasePopulation } = useBearActions()
+  const count = useCount()
+  const { incrementCount, decrementCount } = useCountActions()
   return (
-    <div className="grid min-h-dvh place-content-center p-4">
-      <strong>{bears}</strong>
-      <Button onClick={() => increasePopulation(20)}>Increase Bears</Button>
+    <div className="flex min-h-dvh items-center justify-center">
+      <div className="w-full max-w-56 space-y-4 rounded-md p-2 shadow-md ring-2 ring-primary">
+        <div className="grid h-44 w-full place-content-center rounded-md bg-gradient-to-r from-violet-500 to-fuchsia-500">
+          <h3 className="text-7xl font-black text-white">{count}</h3>
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <Button
+            size="sm"
+            disabled={count === 0}
+            onClick={() => decrementCount()}
+          >
+            Decrement
+          </Button>
+          <Button size="sm" onClick={() => incrementCount()}>
+            Increment
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
