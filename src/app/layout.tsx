ec3e52245087c092f62fa1e7cce539fae02f9d79
Faisal Tariq <faisaltariq183@gmail.com>
2024-09-19 00:10:16 +0400

    chore: layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-dvh bg-background font-sans antialiased', fontSans.variable)}>
+      <body
+        className={cn(
+          'min-h-dvh bg-background font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
