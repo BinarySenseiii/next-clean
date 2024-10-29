13a31bef4eeba61cd150c0eab3acd07ec67bd226
Faisal Tariq <faisaltariq183@gmail.com>
2024-10-29 16:28:51 +0400

    feat: editor-config

--- src/core/constant/env.ts
+++ src/core/constant/env.ts
@@ -2,15 +2,29 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const env = createEnv({
+    /*
+     * Serverside Environment variables, not available on the client.
+     * Will throw if you access these variables on the client.
+     */
     server: {
+        DATABASE_URL: z.string().url(),
         NODE_ENV: z.enum(['development', 'test', 'production'])
     },
 
-    client: {},
+    /*
+     * Environment variables available on the client (and server).
+     *
+     * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+     */
+    client: {
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string()
+    },
 
     skipValidation: !!process.env.SKIP_ENV_VALIDATION,
     emptyStringAsUndefined: true,
     runtimeEnv: {
-        NODE_ENV: process.env.NODE_ENV
+        DATABASE_URL: process.env.DATABASE_URL,
+        NODE_ENV: process.env.NODE_ENV,
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
     }
 });
